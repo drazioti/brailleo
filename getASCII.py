b25284101e14d94897d552b149e774baaca2e7e3
draziotis <drazioti@gmail.com>
2018-12-07 01:58:08 +0200

    changes to README

---
+++ getASCII.py
@@ -0,0 +1,14 @@
+import getch
+
+# prints out braille characters's correspondant ASCII code equivalents
+
+# sstring = "⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
+#for char in string:
+#  print(ord(char))
+print("every keypress will return it's correspondant ASCII code\npress esc to exit")
+while True:
+    key = ord(getch.getch())
+    print(key)
+    if key == 27:
+        break
+
